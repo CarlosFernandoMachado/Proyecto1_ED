15d5f435e939db0898935bcd578e76281d637d26
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2016-08-20 13:11:46 -0600

    Primer Commit
    Clase main creada, y clase lista abstracta

---
+++ Proyecto_1/src/proyecto_1/ADTList.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_1;
+
+/**
+ *
+ * @author carlos
+ */
+public abstract class ADTList {
+    protected int size;
+
+    public ADTList(int size) {
+        this.size = size;
+    }
+
+    public ADTList() {
+        size = 0;
+    }
+    
+    abstract boolean insert(Object element,int position);
+    
+    abstract Object first();
+    
+    abstract Object last();
+    
+    public int Size(){
+        return size;
+    }
+    abstract int capacity();
+    
+    abstract Object remove(int position);
+    
+    public boolean isEmpty(){
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    abstract boolean isFull();
+    
+    abstract int indexOf(Object element);
+    
+    abstract Object get(int position);
+    
+    abstract void clear();
+}
